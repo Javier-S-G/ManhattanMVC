9bf9deddb811d480f1c290a6a9acdf63e8b9790c
JavierSG <jsanudo@gteam.es>
2021-09-23 09:17:13 +0200

    Add project files.

---
+++ ManhattanMVC/Controllers/MoviesController.cs
@@ -0,0 +1,55 @@
+﻿using ManhattanMVC.ViewModels;
+using ManhattanMVC.Views.Movie;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ManhattanMVC.Controllers
+{
+    public class DefaultController : Controller
+    {
+        private List<Movie> _movies = new List<Movie>();
+
+        // GET: Default
+        public ActionResult Random()
+        {
+            var movie = new Movie() { Name = "Not that Fast and Furious 2" };
+            var customers = new List<Customer>
+            {
+                new Customer{Name="C1"},
+                new Customer{Name="C2"},
+            };
+            var viewModel = new RandomMovieViewModel
+            {
+                Movie = movie,
+                Customers = customers
+            };
+            return View(viewModel);
+        }
+
+        // algo asi? no sexd
+        public ActionResult ListingMovies()
+        {
+            var movie1 = new Movie() { Name = "Not that Fast and Furious" };
+            var movie2 = new Movie() { Name = "The ring of the lords" };
+            var movie3 = new Movie() { Name = "Unterminated 2" };
+            _movies.Add(movie1);
+            _movies.Add(movie2);
+            _movies.Add(movie3);
+
+            var customers = new List<Customer>
+            {
+                new Customer{Name="C1"},
+                new Customer{Name="C2"},
+            };
+            var viewModel = new RandomMovieViewModel
+            {
+                Movie = movie1,
+                Customers = customers
+            };
+            return View(viewModel);
+        }
+    }
+}
---
+++ ManhattanMVC/ViewModels/RandomMovieViewModel.cs
@@ -0,0 +1,14 @@
+﻿using ManhattanMVC.Views.Movie;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ManhattanMVC.ViewModels
+{
+    public class RandomMovieViewModel
+    {
+        public Movie Movie { get; set; }
+        public List<Customer> Customers { get; set; }
+    }
+}
